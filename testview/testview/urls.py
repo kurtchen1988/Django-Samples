5432b86a3b789b25c7cd2877a720e31cf91e1e0a
bantu <76251098@qq.com>
2018-11-20 19:42:17 +0800

    2018-11-20

--- testview/testview/urls.py
+++ testview/testview/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('viewthis.urls'))
+    path('', include('viewthis.urls')),
+    path('user/', include('user.urls'))
 ]
--- testview/viewthis/admin.py
+++ testview/viewthis/admin.py
@@ -16,6 +16,35 @@ class ProductAdmin(admin.ModelAdmin):
 	# readonly_fields=['name']
 	list_display.append('colored_type')
 
+	def get_queryset(self, request):
+		qs = super(ProductAdmin, self).get_queryset(request)
+		if request.user.is_superuser:
+			return qs
+		else:
+			return qs.filter(id__lt=6)
+
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+		if db_field.name == 'type':
+			if not request.user.is_superuser:
+				kwargs["queryset"] = Type.objects.filter(id__lt=4)
+		return super(admin.ModelAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+	def save_model(self, request, obj, form, change):
+		if change:
+			user = request.user
+			name = self.model.objects.get(pk=obj.pk).name
+			weight = form.cleaned_data['weight']
+			f = open('MyDjango_log.txt','a')
+			f.write('产品: '+str(name)+', 被用户：'+str(user)+' 修改 '+'\r\n')
+			f.close()
+		else:
+			pass
+		super(ProductAdmin, self).save_model(request, obj, form, change)
+
+	def delete_model(self, request, obj):
+		pass
+		super(ProductAdmin, self).delete_model(request, obj)
+
 @admin.register(Type)
 class TypeAdmin(admin.ModelAdmin):
 	list_display = ['id','type_name']
